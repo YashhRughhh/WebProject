947b59dcd958f662d1ed76fab4273ac5ffb092c5
YashhRughhh <165584010+YashhRughhh@users.noreply.github.com>
2024-04-04 14:35:25 +0530

    Add files via upload

---
+++ LanguageChange.js
@@ -0,0 +1,34 @@
+function langChange() {
+    var languages = document.getElementById("languages");
+    var language = languages.options[languages.selectedIndex].value;
+    var codeArea = document.getElementById("codeArea");
+    if (language.localeCompare("C")==0) {
+        codeArea.value='';
+        codeArea.value+='#include<stdio.h>\n\n';
+        codeArea.value+='int main () {\n';
+        codeArea.value+='printf("Hello World!");\n';
+        codeArea.value+='return 0;\n';
+        codeArea.value+='}';
+    }
+    else if (language.localeCompare("C++")==0) {
+        codeArea.value='';
+        codeArea.value+='#include<iostream>\n';
+        codeArea.value+='using namespace std;\n\n';
+        codeArea.value+='int main () {\n';
+        codeArea.value+='cout<<"Hello World!";\n';
+        codeArea.value+='return 0;\n';
+        codeArea.value+='}';
+    }
+    else if (language.localeCompare("Java")==0) {
+        codeArea.value='';
+        codeArea.value+='class Main{\n';
+        codeArea.value+='public static void main(String args[]) {\n';
+        codeArea.value+='System.out.println("Hello World!");\n';
+        codeArea.value+='}\n';
+        codeArea.value+='}';
+    }
+    else {
+        codeArea.value='';
+        codeArea.value+='print("Hello World!")';
+    }
+}
